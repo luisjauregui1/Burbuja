f42af7d63f8508aee76723e1098fa6f0fa4b133f
Gustavo Jauregui <sr1176626173@gmail.com>
2023-04-23 13:24:18 -0600

    modificacion de Burbuja.py

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,15 +1,16 @@
-lista_num = [9,8,7,6,5,4,3,2,1]
+lista_num = [9,8,7,6,5,4,3,2,1] # Lista de numeros desordenados
 
-lista_len = len(lista_num)
+lista_len = len(lista_num) # Usamos len para saber el largo de la lista
 
-def burbuja(lista):
-    for i in range(0,lista_len-1):
-        for j in range(0,lista_len-1):
-            if lista_num[j] > lista_num[j+1]:
-                lista_num[j+1], lista_num[j] = lista_num[j], lista_num[j+1]
+# Funcion burbuja
+def burbuja(lista):  
+    for i in range(0,lista_len-1): # restamos uno a lista_len para que no pase del rango.
+        for j in range(0,lista_len-1): # lo mismo para el siguiente for
+            if lista_num[j] > lista_num[j+1]: # hacemos una comparación entre indices
+                lista_num[j+1], lista_num[j] = lista_num[j], lista_num[j+1] # hacemos el intercambio si se cumple la condición. 
         
-
-print(f'lista de numeros original {lista_num}')
-print("Ordenamieto Burbuja")
-burbuja(lista_num)
-print(f"Esta es lista ordenada {lista_num}")
+     
+print("Método Burbuja".center(52, "#")) # Titulo
+print(f'lista de numeros original {lista_num}') # Imprimimos la lista original
+burbuja(lista_num) # Llamamos la función y como parámetro pasamos la lista original
+print(f"lista ordenada {lista_num}") # Imprimimos lista ordenada
